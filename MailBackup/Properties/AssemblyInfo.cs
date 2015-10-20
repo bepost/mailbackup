3f1b87e3324727326052e639040e526f3cec97de
Joep Beusenberg <git@bepost.nl>
2015-10-20 22:28:03 +0200

    Initial release build

--- MailBackup/Properties/AssemblyInfo.cs
+++ MailBackup/Properties/AssemblyInfo.cs
@@ -6,13 +6,17 @@ using CommandLine;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MailBackup")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("A tool to sync an imap-compatible mailbox to local files, in order to include them in a backup set.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+[assembly: AssemblyCompany("Bepost")]
 [assembly: AssemblyProduct("MailBackup")]
 [assembly: AssemblyCopyright("Copyright © Bepost 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+
